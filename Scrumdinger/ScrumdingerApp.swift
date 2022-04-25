d6b729ac9688a8fff715093f13a8377a3ffb892e
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-04-25 14:11:02 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jeremy Duncan on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
